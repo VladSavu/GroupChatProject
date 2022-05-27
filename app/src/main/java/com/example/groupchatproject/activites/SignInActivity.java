98121bfb0682c3182406533ce39453ea6a26eef7
VladSavu <476636@student.saxion.nl>
2022-05-27 20:18:54 +0200

    MultiDex & Dummy Data

--- app/src/main/java/com/example/groupchatproject/activites/SignInActivity.java
+++ app/src/main/java/com/example/groupchatproject/activites/SignInActivity.java
@@ -4,9 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.groupchatproject.R;
 import com.example.groupchatproject.databinding.ActivitySignInBinding;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
 
 public class SignInActivity extends AppCompatActivity {
 
@@ -23,6 +27,22 @@ public class SignInActivity extends AppCompatActivity {
     private void setListeners(){
         binding.createNewAccountText.setOnClickListener(view -> {
             startActivity(new Intent(getApplicationContext(), SignUpActivity.class));
+        });
+
+        binding.signInButton.setOnClickListener(view -> {
+            addDataToFirestore();
+        });
+    }
+
+    private void addDataToFirestore(){
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        HashMap<String, Object> data = new HashMap<>();
+        data.put("first_name", "Vlad");
+        data.put("ladt_name", "savu");
+        database.collection("users").add(data).addOnSuccessListener(documentReference -> {
+            Toast.makeText(getApplicationContext(), "Added data succcessfully", Toast.LENGTH_SHORT).show();
+        }).addOnFailureListener(exception -> {
+            Toast.makeText(getApplicationContext(), "Added NOT added" + exception.getMessage(), Toast.LENGTH_SHORT).show();
         });
     }
 }
