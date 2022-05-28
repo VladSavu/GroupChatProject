c89d6feb467a0662c22195c723473cf9c92543d1
VladSavu <476636@student.saxion.nl>
2022-05-28 20:56:19 +0200

    Chat implementation & UI

--- app/src/main/java/com/example/groupchatproject/activites/ChatActivity.java
+++ app/src/main/java/com/example/groupchatproject/activites/ChatActivity.java
@@ -6,19 +6,26 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Base64;
+import android.view.View;
 
-import com.example.groupchatproject.R;
 import com.example.groupchatproject.databinding.ActivityChatBinding;
+import com.example.groupchatproject.models.ChatMessage;
 import com.example.groupchatproject.models.User;
 import com.example.groupchatproject.utilities.Constants;
 import com.example.groupchatproject.utilities.PreferenceManager;
-import com.google.common.base.CharMatcher;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 
 
 public class ChatActivity extends AppCompatActivity {
@@ -38,6 +45,7 @@ public class ChatActivity extends AppCompatActivity {
         setListeners();
         loadReceiverDetails();
         init();
+        listenMessages();
     }
 
     private void init(){
@@ -58,6 +66,47 @@ public class ChatActivity extends AppCompatActivity {
         binding.inputMessage.setText(null);
     }
 
+    private void listenMessages(){
+        database.collection(Constants.KEY_COLLECTION_CHAT).whereEqualTo(Constants.KEY_SENDER_ID, preferenceManager.getString(Constants.KEY_USER_ID))
+                .whereEqualTo(Constants.KEY_RECEIVER_ID, receiverUser.id)
+                .addSnapshotListener(eventListener);
+
+        database.collection(Constants.KEY_COLLECTION_CHAT)
+                .whereEqualTo(Constants.KEY_SENDER_ID, receiverUser.id)
+                .whereEqualTo(Constants.KEY_RECEIVER_ID, preferenceManager.getString(Constants.KEY_USER_ID))
+                .addSnapshotListener(eventListener);
+    }
+
+    private final EventListener<QuerySnapshot> eventListener = (value, error) -> {
+        if (error != null){
+          return;
+        }
+        if(value != null){
+          int count = chatMessages.size();
+          for (DocumentChange documentChange : value.getDocumentChanges()){
+              if(documentChange.getType() == DocumentChange.Type.ADDED){
+                  ChatMessage chatMessage = new ChatMessage();
+                  chatMessage.senderId = documentChange.getDocument().getString(Constants.KEY_SENDER_ID);
+                  chatMessage.receiverId = documentChange.getDocument().getString(Constants.KEY_RECEIVER_ID);
+                  chatMessage.message = documentChange.getDocument().getString(Constants.KEY_MESSAGE);
+                  chatMessage.dateTime = getReadableDateTime(documentChange.getDocument().getDate(Constants.KEY_TIMESTAMP));
+                  chatMessage.dateObject = documentChange.getDocument().getDate(Constants.KEY_TIMESTAMP);
+                  chatMessages.add(chatMessage);
+              }
+          }
+            Collections.sort(chatMessages, Comparator.comparing(obj -> obj.dateObject));
+          if (count == 0){
+              chatAdapter.notifyDataSetChanged();
+          }else{
+              chatAdapter.notifyItemRangeInserted(chatMessages.size(), chatMessages.size());
+              binding.chatRecyclerView.smoothScrollToPosition(chatMessages.size() - 1);
+          }
+
+          binding.chatRecyclerView.setVisibility(View.VISIBLE);
+        }
+
+    };
+
     private Bitmap getBitmapFromEncodedString(String encodedImage){
         byte[] bytes = Base64.decode(encodedImage, Base64.DEFAULT);
         return BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
@@ -73,5 +122,9 @@ public class ChatActivity extends AppCompatActivity {
             onBackPressed();
         });
         binding.layoutSend.setOnClickListener(view -> sendMessage());
+    }
+
+    private String getReadableDateTime(Date date){
+        return new SimpleDateFormat("MMMM dd, yyyy - hh:mm a", Locale.getDefault()).format(date);
     }
 }
