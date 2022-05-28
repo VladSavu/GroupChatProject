5337eb7d9fb923d71fe1d7c16cf85ccc8ad58d1f
VladSavu <476636@student.saxion.nl>
2022-05-28 16:48:33 +0200

    Sign out Firestore FCM token

--- app/src/main/java/com/example/groupchatproject/MainActivity.java
+++ app/src/main/java/com/example/groupchatproject/MainActivity.java
@@ -2,20 +2,24 @@ package com.example.groupchatproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 
+import com.example.groupchatproject.activites.SignInActivity;
 import com.example.groupchatproject.databinding.ActivityMainBinding;
 import com.example.groupchatproject.utilities.Constants;
 import com.example.groupchatproject.utilities.PreferenceManager;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.messaging.FirebaseMessaging;
 
 import android.util.Base64;
 import android.widget.Toast;
+
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -31,6 +35,11 @@ public class MainActivity extends AppCompatActivity {
         preferenceManager = new PreferenceManager(getApplicationContext());
         loadUserData();
         getToken();
+        setListeners();
+    }
+
+    private void setListeners(){
+        binding.imageProfile.setOnClickListener(view -> signOut());
     }
 
     private void loadUserData(){
@@ -56,5 +65,19 @@ public class MainActivity extends AppCompatActivity {
         documentReference.update(Constants.KEY_FCM_TOKEN, token).addOnSuccessListener(s -> {
             showToast("Token updates successfully");
         }).addOnFailureListener(e -> showToast("Unable to update token"));
+    }
+
+    private void signOut(){
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        DocumentReference documentReference = database.collection(Constants.KEY_COLLECTION_USERS).document(
+                preferenceManager.getString(Constants.KEY_USER_ID)
+        );
+        HashMap<String, Object> updates = new HashMap<>();
+        updates.put(Constants.KEY_FCM_TOKEN, FieldValue.delete());
+        documentReference.update(updates)
+                .addOnSuccessListener(s -> {
+                    preferenceManager.clear();
+                    startActivity(new Intent(getApplicationContext(), SignInActivity.class));
+                }).addOnFailureListener(e -> showToast("Unable to log out"));
     }
 }
